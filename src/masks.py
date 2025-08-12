a5714d7cd874d6b936362fc4810cd7baf89b3b90
GalinaUm <galinaumerzakhova@yandex.ru>
2025-08-12 21:52:23 +0300

    fixing errors

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
 def get_mask_card_number(number_card: int) -> str:
-    """ Функция, которая превращает номер карты в маску номера карты """
+    """Функция, которая превращает номер карты в маску номера карты"""
     number_card_stroke = str(number_card)
     return f"{number_card_stroke[0:4]} {number_card_stroke[4:6]}** **** {number_card_stroke[12:]}"
 
 
 def get_mask_account(number_account: int) -> str:
-    """ Функция, которая превращает номер счетв в маску номера счета """
+    """Функция, которая превращает номер счета в маску номера счета"""
     number_account_stroke = str(number_account)
     return f"**{number_account_stroke[16:]}"
--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,27 @@
-from masks import get_mask_card_number, get_mask_account
+from .masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(number: str) -> str:
-    number_digit = ''
+    """Функция, которая создает маску номера или счета"""
+    number_digit = ""
+    number_digit_int = int(number_digit)
     for symbol in number:
         if symbol.isdigit():
             number_digit += symbol
     if len(number_digit) == 16:
-        mask_card_number = get_mask_card_number(number_digit)
-        return f'{number[0:-16]}{mask_card_number}'
+        mask_card_number = get_mask_card_number(number_digit_int)
+        return f"{number[0:-16]}{mask_card_number}"
     elif len(number_digit) == 20:
-        mask_card_number = get_mask_account(number_digit)
-        return f'{number[0:-20]}{mask_card_number}'
+        mask_card_number = get_mask_account(number_digit_int)
+        return f"{number[0:-20]}{mask_card_number}"
     else:
-        return 'У вас не карта, и не счёт!'
+        return "У вас не карта, и не счёт!"
 
 
 def get_date(date_unformatted: str) -> str:
-    date_cut_list = date_unformatted[:10].split('-')
+    """Функция, которая форматирует дату"""
+    date_cut_list = date_unformatted[:10].split("-")
     return ".".join([date_cut_list[1], date_cut_list[2], date_cut_list[0]])
 
 
 print(get_date("2024-03-11T02:26:18.671407"))
-
