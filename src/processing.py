c69f9fb7cde5a55384715e8e38fc4e40cf4c5459
GalinaUm <galinaumerzakhova@yandex.ru>
2025-08-13 22:11:11 +0300

    function filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,14 @@
+def filter_by_state(list_not_filtered: list, state: str = 'EXECUTED') -> list:
+    """Функция, которая фильтрует списки словарей по нужному параметру"""
+    list_filtered = []
+    for dict_item in list_not_filtered:
+        for item in dict_item.values():
+            if item == state:
+                list_filtered.append(dict_item)
+            else:
+                continue
+    return list_filtered
+
+
+state_2 = 'CANCELED'
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
