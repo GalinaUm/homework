accce3ca30acfcf831839df7ad06d201c17632e7
GalinaUm <galinaumerzakhova@yandex.ru>
2025-08-21 21:54:41 +0300

    rename

---
+++ tests/test_masks.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+@pytest.fixture
+def number_card():
+    return 7000792289606361
+
+
+@pytest.mark.parametrize("expected", ["7000 79** **** 6361"])
+def test_get_mask_card_number(number_card, expected):
+    assert get_mask_card_number(number_card) == expected
+
+
+
+
+
+
+
