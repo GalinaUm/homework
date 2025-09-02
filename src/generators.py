fcadab7d17f0f7b5188a0566b0c5f4a318c0cc1b
GalinaUm <galinaumerzakhova@yandex.ru>
2025-09-02 21:02:03 +0300

    generators card number generator func

--- src/generators.py
+++ src/generators.py
@@ -18,3 +18,21 @@ def transaction_descriptions(transactions: list[dict]) -> Iterator:
     for transaction in transactions:
         description = transaction.get("description")
         yield description
+
+
+def card_number_generator(start: int = 1, end: int = 9999999999999999) -> Iterator[str]:
+    """Генератор номеров банковских карт в формате XXXX XXXX XXXX XXXX"""
+    if start < 1:
+        raise ValueError("Начальный номер должен быть не менее 1")
+    if end > 9999999999999999:
+        raise ValueError("Конечный номер должен менее 16 символов")
+    if start > end:
+        raise ValueError("Начальный номер должен быть меньше или равен конечному")
+
+    current = start
+    while current <= end:
+        card_number = str(current).zfill(16)
+
+        formatted = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+        yield formatted
+        current += 1
