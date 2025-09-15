03fa9b7bf368c6a90cd0d42aa8dd4db7a987a2f7
GalinaUm <galinaumerzakhova@yandex.ru>
2025-09-15 18:44:24 +0300

    errors

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,10 @@
 import datetime
 import logging
 
-
-logger = logging.getLogger('masks')
+logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('../logs/masks.log')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("../logs/masks.log")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -14,12 +13,12 @@ def get_mask_card_number(number_card: int) -> str:
     """Функция, которая превращает номер карты в маску номера карты"""
     today = datetime.datetime.today()
 
-    logger.warning(f'Номер {number_card} неправильный')
+    logger.warning(f"Номер {number_card} неправильный")
     if len(str(number_card)) != 16:
-        logger.error('Произошла ошибка')
+        logger.error("Произошла ошибка")
         raise ValueError("Неправильная длина номера")
 
-    logger.info(f'Создается маска для номера карты {number_card}')
+    logger.info(f"Создается маска для номера карты {number_card}")
     number_card_string = str(number_card)
     return f"{number_card_string[0:4]} {number_card_string[4:6]}** **** {number_card_string[12:]}"
 
@@ -27,12 +26,12 @@ def get_mask_card_number(number_card: int) -> str:
 def get_mask_account(number_account: int) -> str:
     """Функция, которая превращает номер счета в маску номера счета"""
 
-    logger.warning(f'Номер {number_account} неправильный')
+    logger.warning(f"Номер {number_account} неправильный")
     if len(str(number_account)) != 20:
-        logger.error('Произошла ошибка')
+        logger.error("Произошла ошибка")
         raise ValueError("Неправильная длина номера")
 
-    logger.info(f'Создается маска для номера аккаунта {number_account}')
+    logger.info(f"Создается маска для номера аккаунта {number_account}")
     number_account_stroke = str(number_account)
     return f"**{number_account_stroke[16:]}"
 
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,13 @@
+import datetime
 import json
+import logging
 import os
 from typing import Any
-import datetime
-import logging
 
-
-logger = logging.getLogger('utils')
+logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('../logs/utils.log')
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("../logs/utils.log")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -16,21 +15,20 @@ logger.addHandler(file_handler)
 def read_transactions(file_path: str) -> None | list[Any] | list:
     """Читает транзакции из файла"""
 
-
     if not os.path.exists(file_path):
-        logger.info('Файла не существует или он пустой')
+        logger.info("Файла не существует или он пустой")
         return []
 
     try:
-        logger.info('Данные считываются из файла')
+        logger.info("Данные считываются из файла")
         with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if not data or not isinstance(data, list):
                 return []
             return data
     except json.JSONDecodeError:
-        logger.error('Данные имеют неправильный вид')
+        logger.error("Данные имеют неправильный вид")
         print("Invalid JSON data.")
 
-print(read_transactions("../data/operations.json"))
 
+print(read_transactions("../data/operations.json"))
