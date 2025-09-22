60377e9d059f534566caa1ab8ce41bc25ba518ad
GalinaUm <galinaumerzakhova@yandex.ru>
2025-09-22 10:51:01 +0300

    fixixng path logging masks utils

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ from typing import Any
 
 logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("../logs/utils.log")
+file_handler = logging.FileHandler("F:/Galina/_python/_homework/logs/masks.log")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
